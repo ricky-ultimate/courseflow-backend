3fc91422b635444d21f78f78d7eb5b9dc582d7d6
Ricky Ultimate <rickyogigi@gmail.com>
2025-06-17 22:00:04 +0100

    feat: more updates

--- src/schedules/dto/create-schedule.dto.ts
+++ src/schedules/dto/create-schedule.dto.ts
@@ -3,28 +3,28 @@ import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
 import { DayOfWeek, ClassType } from '../../generated/prisma';
 
 export class CreateScheduleDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'CS101'})
+  @IsString({message: 'Course code must be a string'})
+  @IsNotEmpty({message: 'Course code is required'})
   courseCode: string;
 
   @ApiProperty({ enum: DayOfWeek })
   @IsEnum(DayOfWeek)
   dayOfWeek: DayOfWeek;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: '08:00'})
+  @IsString({message: 'Start time must be a string'})
+  @IsNotEmpty({message: 'Start time is required'})
   startTime: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: '09:00'})
+  @IsString({message: 'End time must be a string'})
+  @IsNotEmpty({message: 'End time is required'})
   endTime: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'Room 101'})
+  @IsString({message: 'Venue must be a string'})
+  @IsNotEmpty({message: 'Venue is required'})
   venue: string;
 
   @ApiProperty({ enum: ClassType, default: 'LECTURE' })
