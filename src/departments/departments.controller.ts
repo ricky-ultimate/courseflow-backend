57c00ba34b95e19e07431a67873a274589282160
Ricky Ultimate <rickyogigi@gmail.com>
2025-06-17 21:15:58 +0100

    chore: update the department poperations

--- src/departments/departments.controller.ts
+++ src/departments/departments.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { DepartmentsService } from './departments.service';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
 import { BaseController } from '../common/controllers/base.controller';
 import { CrudRoles } from '../common/decorators/crud-roles.decorator';
 import { Department, Role } from '../generated/prisma';
+import { Public } from '../common/decorators/public.decorator';
 
 @ApiTags('Departments')
 @Controller('departments')
@@ -25,7 +35,22 @@ export class DepartmentsController extends BaseController<
     super(departmentsService);
   }
 
+  @Post()
+  @ApiOperation({ summary: 'Create a new department' })
+  @ApiBody({ type: CreateDepartmentDto })
+  create(@Body() createDto: CreateDepartmentDto) {
+    return this.departmentsService.create(createDto);
+  }
+
+  @Get()
+  @Public()
+  @ApiOperation({ summary: 'Get all departments' })
+  findAll(@Query() query?: any) {
+    return this.departmentsService.findAll(query);
+  }
+
   @Get(':code')
+  @Public()
   @ApiOperation({ summary: 'Get department by code' })
   @ApiParam({ name: 'code', description: 'Department code' })
   findOne(@Param('code') code: string) {
@@ -35,6 +60,7 @@ export class DepartmentsController extends BaseController<
   @Patch(':code')
   @ApiOperation({ summary: 'Update department by code' })
   @ApiParam({ name: 'code', description: 'Department code' })
+  @ApiBody({ type: UpdateDepartmentDto })
   update(@Param('code') code: string, @Body() updateDto: UpdateDepartmentDto) {
     return this.departmentsService.update(code, updateDto);
   }
--- src/departments/dto/create-department.dto.ts
+++ src/departments/dto/create-department.dto.ts
@@ -2,13 +2,13 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateDepartmentDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'Computer Science'})
+  @IsString({message: 'Name must be a string'})
+  @IsNotEmpty({message: 'Name is required'})
   name: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'CS'})
+  @IsString({message: 'Code must be a string'})
+  @IsNotEmpty({message: 'Code is required'})
   code: string;
 }
