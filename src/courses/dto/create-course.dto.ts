a8ce605859d0e2c7510399d11c923cae8d21af9e
Ricky Ultimate <rickyogigi@gmail.com>
2025-06-17 22:00:04 +0100

    feat: more updates

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -3,22 +3,22 @@ import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
 import { Level } from '../../generated/prisma';
 
 export class CreateCourseDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'CS101'})
+  @IsString({message: 'Code must be a string'})
+  @IsNotEmpty({message: 'Code is required'})
   code: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'Introduction to Computer Science'})
+  @IsString({message: 'Name must be a string'})
+  @IsNotEmpty({message: 'Name is required'})
   name: string;
 
   @ApiProperty({ enum: Level })
   @IsEnum(Level)
   level: Level;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
+  @ApiProperty({example: 'CS'})
+  @IsString({message: 'Department code must be a string'})
+  @IsNotEmpty({message: 'Department code is required'})
   departmentCode: string;
 }
