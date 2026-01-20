290ff5d419ac689681a8da1efa91b9098dfac844
Ricky Ultimate <rickyogigi@gmail.com>
2026-01-20 15:28:16 +0100

    feat: minor updates

--- src/modules/exams/exams.controller.ts
+++ src/modules/exams/exams.controller.ts
@@ -1,30 +1,74 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ExamsService } from './exams.service';
 import { CreateExamDto } from './dto/create-exam.dto';
-import { RolesGuard } from '../../common/guards/roles.guard';
-import { Roles } from '../../common/decorators/roles.decorator';
-import { Role } from '../../generated/prisma';
+import { UpdateExamDto } from './dto/update-exam.dto';
+import { BaseController } from '../../common/controllers/base.controller';
+import { CrudRoles } from '../../common/decorators/crud-roles.decorator';
+import { ExamSchedule, Role } from '../../generated/prisma';
+import { PaginationOptions } from '../../common/interfaces/base-service.interface';
+import {
+  ApiCreateExam,
+  ApiGetExams,
+  ApiGetExamById,
+  ApiUpdateExam,
+  ApiDeleteExam,
+} from './decorators/exam-api.decorator';
 
 @ApiTags('Exams')
-@ApiBearerAuth('JWT-auth')
 @Controller('exams')
-@UseGuards(RolesGuard)
-export class ExamsController {
-  constructor(private readonly examsService: ExamsService) {}
+@CrudRoles({
+  entity: 'exam',
+  create: [Role.ADMIN],
+  read: [Role.ADMIN, Role.LECTURER, Role.STUDENT],
+  update: [Role.ADMIN],
+  delete: [Role.ADMIN],
+})
+export class ExamsController extends BaseController<
+  ExamSchedule,
+  CreateExamDto,
+  UpdateExamDto
+> {
+  constructor(private readonly examsService: ExamsService) {
+    super(examsService);
+  }
 
   @Post()
-  @Roles(Role.ADMIN)
-  @ApiOperation({ summary: 'Schedule a new exam' })
-  @ApiResponse({ status: 201, description: 'Exam scheduled successfully' })
-  @ApiResponse({ status: 409, description: 'Conflict (Venue/College/Time)' })
-  create(@Body() createExamDto: CreateExamDto) {
-    return this.examsService.create(createExamDto);
+  @ApiCreateExam()
+  create(@Body() createDto: CreateExamDto) {
+    return this.examsService.create(createDto);
   }
 
   @Get()
-  @ApiOperation({ summary: 'Get all scheduled exams' })
-  findAll() {
-    return this.examsService.findAll();
+  @ApiGetExams()
+  findAll(@Query() query?: PaginationOptions) {
+    return this.examsService.findAll(query);
+  }
+
+  @Get(':id')
+  @ApiGetExamById()
+  findOne(@Param('id') id: string) {
+    return this.examsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ApiUpdateExam()
+  update(@Param('id') id: string, @Body() updateDto: UpdateExamDto) {
+    return this.examsService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  @ApiDeleteExam()
+  remove(@Param('id') id: string) {
+    return this.examsService.remove(id);
   }
 }
