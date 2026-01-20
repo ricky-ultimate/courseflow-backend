a1b52f1f50612b1aa59f65432785ca368eb19e1d
Ricky Ultimate <rickyogigi@gmail.com>
2026-01-20 14:50:49 +0100

    fix: invalid dto export

--- src/modules/exams/dto/update-exam.dto.ts
+++ src/modules/exams/dto/update-exam.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateExamDto } from './create-exam.dto';
 
-export class UpdateCourseDto extends PartialType(CreateExamDto) {}
+export class UpdateExamDto extends PartialType(CreateExamDto) {}
