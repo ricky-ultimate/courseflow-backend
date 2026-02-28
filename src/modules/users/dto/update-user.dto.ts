abe2dad82b7906c24b1464a3ed9cca2313c0c45a
Ricky Ultimate <rickyogigi@gmail.com>
2026-02-28 03:35:48 +0100

    chore: lint fix

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -3,6 +3,4 @@ import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['password'] as const),
-) {
-    password: any;
-}
+) {}
