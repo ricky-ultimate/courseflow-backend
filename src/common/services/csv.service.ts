faac98ba63390f8b3e92692036132219acb1560d
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-02 23:27:21 +0100

    chore: remove unused method

--- src/common/services/csv.service.ts
+++ src/common/services/csv.service.ts
@@ -120,16 +120,6 @@ export class CsvService {
     return csv;
   }
 
-  validateTimeRange(startTime: string, endTime: string): boolean {
-    const startParts = startTime.split(':');
-    const endParts = endTime.split(':');
-
-    const startMinutes = parseInt(startParts[0]) * 60 + parseInt(startParts[1]);
-    const endMinutes = parseInt(endParts[0]) * 60 + parseInt(endParts[1]);
-
-    return endMinutes > startMinutes;
-  }
-
   createBulkResult<T>(
     created: T[],
     errors: CsvValidationError[],
