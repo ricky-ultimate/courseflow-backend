334b9138f546429055876a895c7b4543e12ad276
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-02 23:32:54 +0100

    chore: remove dead code

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,6 @@
 import {
   Injectable,
   NotFoundException,
-  BadRequestException,
 } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
