7e26cd76f9e182e6c61ce0a436cb54c1a31f1678
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-18 13:57:43 +0100

    chore: add isActive to update users dto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,6 +1,13 @@
 import { PartialType, OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['password'] as const),
-) {}
+) {
+  @ApiProperty({ required: false, example: true })
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
